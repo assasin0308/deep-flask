ce340cad9a6d3f66b07a086fd18c855421ab61c5
史斌 <assasin0308@sina.com>
2020-04-21 00:24:09 +0800

    config & route

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # coding:utf-8
-from flask import Flask,request,render_template,make_response
+from flask import Flask,request,\
+    render_template,make_response,current_app
 
 app = Flask(
     __name__,
@@ -8,13 +9,44 @@ app = Flask(
     template_folder='muban', # 模板文件目录 ,默认 templates
 )   # 同 app = Flask('__main__')
 
+# 配置参数
+# 1. 使用配置文件
+# 使用配置文件配置参数
+    # 1. app.config.from_pyfile("config.cfg")
+
+    # class Config(object):
+    #     DEBUG = True
+    #     NAME = 'assasin'
+    #
+    # app.config.from_object(Config)
+    # 使用对象配置参数  项目推荐
+    # 2. app.config.from_object(Config)
+    # 3. app.config.from_envvar()
+    # 4. 直接操作config的字典对象
+    # app.config["DEBUG"] = True
+
+
+# 路由配置
+# 查看所有路由信息: app.url_map
+# 06  03:12
 
 
 
 @app.route('/')
 def index():
+    # 读取配置参数
+    # 1. 直接从全局对象app的config字典中取值
+    # app.config["DEBUg"]
+    # app.config.get("NAME")
+    # 2. 通过current_app获取
+    # print(current_app.config.get('NAME'))
     return 'hello world'
 
 if __name__ == '__main__':
+    # app.run() 参数配置:
+    # host: '0.0.0.0' 默认 127.0.0.1
+    # port: 5001 默认 5000
+    # debug: True/False 开启/关闭debug模式
+    # print(app.url_map)
     app.run(host='0.0.0.0',debug=True)
 
